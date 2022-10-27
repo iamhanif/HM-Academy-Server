ae1f43d020549baaaced3cc7b0d35d26317e0197
Hanif <hanif.aiubeee@gmail.com>
2022-10-27 23:09:27 +0600

    courses details added

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 app.use(cors())
+
 const courses = require('./data/courses.json')
+const courseDetails = require('./data/courseDetails.json')
+
 
 app.get('/', (req, res) => {
     res.send('courses api is running')
@@ -12,6 +15,16 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+
+app.get('/courseDetails', (req, res) => {
+    res.send(courseDetails)
+})
+
+app.get('/courseDetails/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courseDetails.find(course => course._id === id)
+    res.send(selectedCourse)
 })
 
 app.listen(port, () => {
